e2ea9998403cb621c0864a2fdb405c251c377230
Sebastian Sdorra <s.sdorra@gmail.com>
2020-06-16 16:10:02 +0200

    added lowlight adapter to fix usage with lowligth 1.14.0

--- src/default-highlight.js
+++ src/default-highlight.js
@@ -3,7 +3,18 @@ import defaultStyle from './styles/hljs/default-style';
 import lowlight from 'lowlight';
 import supportedLanguages from './languages/hljs/supported-languages';
 
-const highlighter = highlight(lowlight, defaultStyle);
+// we have to use an adapter, because since lowligth 1.14
+// the parameters are switched (language, code instead code, language).
+const lowlightAdapter = {
+  highlight(code, language) {
+    return lowlight.highlight(language, code).value;
+  },
+  highlightAuto(code) {
+    return lowlight.highlightAuto(code).value;
+  }
+};
+
+const highlighter = highlight(lowlightAdapter, defaultStyle);
 highlighter.supportedLanguages = supportedLanguages;
 
 export default highlighter;
